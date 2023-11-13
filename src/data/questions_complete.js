ceaf4e1e1bd71f385e23d509b2896a0987a56936
Pedro <pedroshalommrn@hotmail.com>
2023-11-13 14:37:12 -0300

    add novas questions

---
+++ src/data/questions_complete.js
@@ -0,0 +1,135 @@
+const data = [
+    {
+      category: "Futebol",
+      questions: [
+        {
+          question: "Em que ano o futebol foi criado?",
+          options: ["1863", "1870", "1930", "1910"],
+          answer: "1863",
+          tip: "É uma tag de uma letra apenas",
+        },
+        {
+          question: "Quando foi a primeira copa do mundo?",
+          options: ["1958", "1938", "1930", "1940"],
+          answer: "1930",
+          tip: "Hyperlink Reference",
+        },
+        {
+          question: "Quem foi o primeiro artilheiro da primeira copa do mundo",
+          options: ["Pelé", "Maradona", "Guillermo Stábile", "Rivelino"],
+          answer: "Guillermo Stábile",
+        },
+        {
+          question: "O Messi é destro ou canhoto?",
+          options: ["Destro", "Canhoto"],
+          answer: "Canhoto",
+        },
+        {
+          question: "Quantos mundiais de clubes tem o Real Madrid? 🤔",
+          options: ["0", "8", "3", "5"],
+          answer: "8",
+        },
+        {
+          question: "Que time tem 6 Copas do Brasil?🏆🏆🏆🏆🏆🏆",
+          options: ["Flamengo", "Cruzeiro", "São Paulo", "Vasco"],
+          answer: "Cruzeiro",
+        },
+        {
+          question: "Quando a Marta começou a jogar futebol no profissional?⚽🏃‍♀️",
+          options: ["2000", "2002", "2007", "1999"],
+          answer: "2000",
+        },
+        {
+          question: "Em qual jogador o Zinédine Zidane deu a cabeçada na final da Copa do Mundo 2006?🤔",
+          options: ["Genaro Gattuso", "Marco Materazzi", "Gianluigi Buffon", "Pepe Guardiolla"],
+          answer: "Marco Materazzi",
+        },
+        {
+          question: "Quem era o goleiro da seleção brasileira na goleada dos 7x1?",
+          options: ["Jefferson", "Marcos", "Júlio César", "Dida"],
+          answer: "Júlio César",
+        },
+        {
+          question: "Quantos gols tem Rogério Ceni?⚽",
+          options: ["131", "100", "200", "99"],
+          answer: "131",
+        },
+        {
+          question: "Quem lesiounou Neymar na copa de 2014",
+          options: ["Carlos Bacca", "Juan Camilo Zúñiga", "Sergio Ramos", "Rames Rodrigues"],
+          answer: "Juan Camilo Zúñiga",
+        },
+      ],
+    },
+    
+    {
+      category: "Novela",
+      questions: [
+        {
+          question: "Qual regra altera a cor de um elemento?",
+          options: ["color", "background-color", "font-size", "transition"],
+          answer: "color",
+          tip: "Cor em inglês",
+        },
+        {
+          question: "Para aumentar a fonte de um elemento utilizamos:",
+          options: ["font", "text-transform", "font-size", "hover"],
+          answer: "font-size",
+        },
+        {
+          question: "A posição que deixa um elemento fixo é a:",
+          options: ["static", "absolute", "fixed", "relative"],
+          answer: "fixed",
+        },
+      ],
+    },
+    {
+      category: "Filmes",
+      questions: [
+        // {
+        //   question: "Qual a sigla de javaScript?",
+        //   options: [
+        //     "es",
+        //     "fs",
+        //     "vs",
+        //     "js",
+        //   ],
+        //   answer: "js",
+        // },
+        // {
+        //   question: "onde programar?",
+        //   options: [
+        //     "1",
+        //     "2",
+        //     "3",
+        //     "4",
+        //   ],
+        //   answer: "1",
+        // },
+        {
+          question: "Com qual instrução declaramos uma constante em JavaScript?",
+          options: ["const", "let", "var", "define"],
+          answer: "const",
+        },
+        {
+          question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+          options: ["string", "number", "boolean", "float"],
+          answer: "float",
+        },
+        {
+          question: "Qual dos métodos a seguir seleciona um elemento?",
+          options: ["querySelector", "parseInt", "sort", "reduce"],
+          answer: "querySelector",
+        },
+        {
+          question:
+            "Qual destas propriedades da a quantidade de elementos de um array?",
+          options: ["qty", "length", "items", "index"],
+          answer: "length",
+        },
+      ],
+    },
+  ];
+  
+  export default data;
+  
